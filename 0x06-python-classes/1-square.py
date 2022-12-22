2d55f1e61366817e50ebaa0eacf274d80b56aa37
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-12-22 01:06:45 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,14 @@
-ajdadf
+#!/usr/bin/python3
+"""Square related feature module."""
+
+
+class Square:
+    """Class that define a Square."""
+
+    def __init__(self, size):
+        """
+            Args:
+                size (int): size initializer
+        """
+
+        self.__size = size
