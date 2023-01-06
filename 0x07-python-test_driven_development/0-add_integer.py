7d8e60ba3dc629b45232a42a508185d357dbbb4e
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2023-01-07 00:57:23 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Addition Module
-    Args:
-        a: first argument; type must be int or float.
-        b: second argument; default is 98. If given must be int or float
+"""
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
 """
 
 
 def add_integer(a, b=98):
     """
-        Returns an addtion integer result
+    Checks if int, otherwise return sum
     """
-    type_list = [int, float]
-    error_msg_a = "a must be an integer"
-    error_msg_b = "b must be an integer"
-
-    if (type(a) not in type_list):
-        raise TypeError(error_msg_a)
-
-    if (type(b) not in type_list):
-        raise TypeError(error_msg_b)
-
-    if (type(a) is float):
+    if type(a) == float or type(b) == float:
         a = int(a)
-    if (type(b) is float):
         b = int(b)
 
-    return (a + b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
