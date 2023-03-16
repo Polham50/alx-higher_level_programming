c55cb45558c346b08f3c53d06d55bc5a82382bd6
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2023-03-16 01:08:03 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
-ajdfadf
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let col = 0; col < this.width; col++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+}
+
+module.exports = Square;
